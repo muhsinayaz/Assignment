6bfe5c411164a28fa87d0458a80f01faf215d0bd
Muhsin Ayaz <zorexq@gmail.com>
2022-11-29 14:00:07 +0300

    assignment

--- 22-09-26/Assignment-2.sql
+++ 22-09-26/Assignment-2.sql
@@ -41,6 +41,40 @@ where a.customer_id in (select distinct  c.customer_id
 
 
 
+
+--v2
+
+WITH T1 AS
+(
+SELECT	DISTINCT A.product_name, D.customer_id, D.first_name, D.last_name
+FROM	product.product A, sale.order_item B, sale.orders C, sale.customer D
+WHERE	A.product_id = B.product_id
+AND		B.order_id = C.order_id
+AND		C.customer_id = D.customer_id
+AND		A.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
+), T2 AS
+(
+SELECT	DISTINCT A.product_name, D.customer_id, D.first_name, D.last_name
+FROM	product.product A, sale.order_item B, sale.orders C, sale.customer D
+WHERE	A.product_id = B.product_id
+AND		B.order_id = C.order_id
+AND		C.customer_id = D.customer_id
+AND		A.product_name = 'Polk Audio - 50 W Woofer - Black'
+)
+SELECT	T1.customer_id, T1.first_name, T1.last_name,
+		ISNULL (NULLIF (ISNULL(T2.first_name, 'No'), T2.first_name) , 'Yes') as is_otherproduct_order
+FROM	T1
+		LEFT JOIN
+		T2
+		ON	T1.customer_id = T2.customer_id
+
+
+
+
+
+
+
+
 -------------------------
 
 --2. Conversion Rate
@@ -81,3 +115,38 @@ select * from actions
 select adv_type,left((COUNT(case when action = 'Order' then 1 end) * 1.0 / count(adv_type)),4) as conversion_rate
 from actions
 group by adv_type
+
+
+--v2-
+
+SELECT * INTO #TABLE1
+FROM
+( VALUES 			
+				(1,'A', 'Left'),
+				(2,'A', 'Order'),
+				(3,'B', 'Left'),
+				(4,'A', 'Order'),
+				(5,'A', 'Review'),
+				(6,'A', 'Left'),
+				(7,'B', 'Left'),
+				(8,'B', 'Order'),
+				(9,'B', 'Review'),
+				(10,'A', 'Review')
+			) A (visitor_id, adv_type, actions)
+WITH T1 AS
+(
+SELECT	adv_type, COUNT (*) cnt_action
+FROM	#TABLE1
+GROUP BY
+		adv_type
+), T2 AS
+(
+SELECT	adv_type, COUNT (actions) cnt_order_actions
+FROM	#TABLE1
+WHERE	actions = 'Order'
+GROUP BY
+		adv_type
+)
+SELECT	T1.adv_type, CAST (ROUND (1.0*T2.cnt_order_actions / T1.cnt_action, 2) AS numeric (3,2)) AS Conversion_Rate
+FROM	T1, T2
+WHERE	T1.adv_type = T2.adv_type
